d2a248f29b3daeadfb895af459b5e0e6747e19b7
RomanVmi <Romarick@xaker.ru>
2015-12-05 23:00:55 +0500

    ScriptsUpdate
    Teleportation , Fading

---
+++ Da4a-project/Assets/Scripts/LocationChanger.cs
@@ -0,0 +1,19 @@
+﻿ using UnityEngine;
+using System.Collections;
+
+public class LocationChanger : MonoBehaviour {
+
+    public Transform teleportTo;
+
+    IEnumerator OnTriggerEnter2D(Collider2D other) {
+
+        ScreenFader sf = GameObject.FindGameObjectWithTag("Fader").GetComponent<ScreenFader>();
+
+        yield return StartCoroutine(sf.FadeToBlack ());
+
+        other.gameObject.transform.position = teleportTo.position;
+        Camera.main.transform.position = teleportTo.position;
+
+        yield return StartCoroutine(sf.FadeToClear());
+    }
+}
